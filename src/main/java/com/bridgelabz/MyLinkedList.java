f182cacdfc4ecee7ddbbc148577f8e9a3bfe21bb
vinay-ryakala <vinayrakyala@gmail.com>
2021-01-13 22:56:53 +0530

    ADDED:Three integers appended to bottom of  linked list

--- src/main/java/com/bridgelabz/MyLinkedList.java
+++ src/main/java/com/bridgelabz/MyLinkedList.java
@@ -21,5 +21,16 @@ public class MyLinkedList {
             this.head.setNext(tempNode);
         }
     }
+    public void append(INode myNode) {
+        if(this.head==null) {
+            this.head=myNode;
+        }
+        if(this.tail==null) {
+            this.tail=myNode;
+        } else {
+            this.tail.setNext(myNode);
+            this.tail=myNode;
+        }
+    }
 
 }
--- src/test/java/com/bridgelabz/MyLinkedListTest.java
+++ src/test/java/com/bridgelabz/MyLinkedListTest.java
@@ -20,5 +20,22 @@ public class MyLinkedListTest {
         Assert.assertTrue(result);
 
     }
+    @Test
+    public void given3NumbersWhenAppendedToLinkedListShouldAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new  MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+
+    }
+
 }
 
